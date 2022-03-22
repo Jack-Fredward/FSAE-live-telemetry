41703458c9fbcf0a297def15def6a9b23039c3a9
Jack-Fredward <johnathan.prideaux@gmail.com>
2022-03-22 16:19:55 -0400

    fixed tracking of read_from_can_demo file

--- Arduino Software/Tests/data_processor/andrews_data_processor/andrews_data_processor.ino
+++ Arduino Software/Tests/data_processor/andrews_data_processor/andrews_data_processor.ino
@@ -5,10 +5,22 @@ int FPSecondByte = 0;
 int FPFirstByte = 0;
 
 struct packetDataStruct {
-int frontBreakPressureFirstByte = 0;
-int frontBreakPressureSecondByte = 0;
-int rearBreakPressureFirstByte = 0;
-int rearBreakPressureSecondByte = 0;
+  int throttlePositionFirstByte;
+  int throttlePositionSecondByte;
+  int engineSpeedFirstByte;
+  int engineSpeedSecondByte;
+  int fuelPressureFirstByte;
+  int fuelPressureSecondByte;
+  int engineOilPressureFirstByte;
+  int engineOilPressureSecondByte;
+  int engineOilTemperatureByte;
+  int coolantTemperatureByte;
+  int gearByte;
+  int exhaustLambdaByte;
+  int frontBrakePressureFirstByte;
+  int frontBrakePressureSecondByte;
+  int rearBrakePressureFirstByte;
+  int rearBrakePressureSecondByte;
 } packetData;
 
 void setup() {
@@ -33,22 +45,15 @@ int convertHexToDec(unsigned char toConvert[20]){
   while(toConvert[lastPos]!=' '){
     lastPos++;
   }
-//  Serial.print("lastPos=");
-//  Serial.print(lastPos);
-//  Serial.println();
   int result = 0;
   for(int i=lastPos; i>0; i--){
-//    Serial.print("Converted char before power function: ")
-//    Serial.print(((char)toConvert[i]- '0'));
     result += ((char)toConvert[i]- '0')*power(10,(lastPos-i));
   }
-//  Serial.print("Int: ");
-//  Serial.println(result);
   return result;
 }
 
 // function to convert kPa to psi (6.894 kPa = 1 psi)
-// Apply to sensors: Fuel Pressure, Oil Pressure, Front & Rear Break pressures
+// Apply to sensors: Fuel Pressure, Oil Pressure, Front & Rear Brake pressures
 float convert_kPa_psi(long rawValue, float divisorConst){
   return ((rawValue/6.89475729)/divisorConst);
 }
@@ -60,17 +65,17 @@ float convert_Hz_rpm(long rawValue, int divisorConst){
 
 // Apply to sensors: Engine Coolant Temp, Engine Oil Temp
 float convert_C_F(long rawValue, float divisorConst){
-  Serial.print("Raw Value: ");
-  Serial.println(rawValue);
+//  Serial.print("Raw Value: ");
+//  Serial.println(rawValue);
   int rawValue_d = rawValue/divisorConst;
-  Serial.print("rawValue / divisor Const: ");
-  Serial.println(rawValue_d);
+//  Serial.print("rawValue / divisor Const: ");
+//  Serial.println(rawValue_d);
   int converted_sensor_C = (rawValue_d*10)-400;
-  Serial.print("converted sensor C: ");
-  Serial.println(converted_sensor_C);
+//  Serial.print("converted sensor C: ");
+//  Serial.println(converted_sensor_C);
   int converted_sensor_F = converted_sensor_C*0.1*(1.8)+32;
-  Serial.print("converted sensor F: ");
-  Serial.println(converted_sensor_F);
+//  Serial.print("converted sensor F: ");
+//  Serial.println(converted_sensor_F);
   return converted_sensor_F;
 }
 
@@ -93,9 +98,6 @@ int andrewsConversion(int firstByte, int secondByte){
   return (((firstByte+16) * 256) + (secondByte+16));
 }
 
-
-
-
 void loop() {
   //clear in_buff from last read
    
@@ -104,154 +106,127 @@ void loop() {
 //  Serial.println((char*)in_buff);
   // send data only when you receive data:
   if (Serial.available() > 0) {
-    unsigned char in_buff[10];
-    for(int j = 0; j<10; j++){
+    unsigned char in_buff[5];
+    for(int j = 0; j<5; j++){
       in_buff[j]= ' ';
     }
     // read the incoming byte:
     // incomingByte = Serial.read();
-    Serial.readBytesUntil('\n',in_buff,10);
+    Serial.readBytesUntil('\n',in_buff,5);
     Serial.println((char*)in_buff);
+    if ((char)in_buff[0] == 't'){
+      packetData.throttlePositionFirstByte = convertHexToDec(in_buff);
+    }
+    if ((char)in_buff[0] == 'T'){
+      packetData.throttlePositionSecondByte = convertHexToDec(in_buff);
+    }
+    if ((char)in_buff[0] == 'e'){
+      packetData.engineSpeedFirstByte = convertHexToDec(in_buff);
+    }
+    if ((char)in_buff[0] == 'E'){
+      packetData.engineSpeedSecondByte = convertHexToDec(in_buff);
+    }
+    if ((char)in_buff[0] == 'u'){
+      packetData.fuelPressureFirstByte = convertHexToDec(in_buff);
+    }
+    if ((char)in_buff[0] == 'U'){
+      packetData.fuelPressureSecondByte = convertHexToDec(in_buff);
+    }
+    if ((char)in_buff[0] == 'p'){
+      packetData.engineOilPressureFirstByte = convertHexToDec(in_buff);
+    }
+    if ((char)in_buff[0] == 'P'){
+      packetData.engineOilPressureSecondByte = convertHexToDec(in_buff);
+    }
+    if ((char)in_buff[0] == 'O'){
+      packetData.engineOilTemperatureByte = convertHexToDec(in_buff);
+    }
+    if ((char)in_buff[0] == 'W'){
+      packetData.coolantTemperatureByte = convertHexToDec(in_buff);
+    }
+    if ((char)in_buff[0] == 'G'){
+      packetData.gearByte = convertHexToDec(in_buff);
+    }
+    if ((char)in_buff[0] == 'L'){
+      packetData.exhaustLambdaByte = convertHexToDec(in_buff);
+    }
     if ((char)in_buff[0] == 'F') {
-//      Serial.println((char*)in_buff);
-      packetData.frontBreakPressureSecondByte = convertHexToDec(in_buff);
-//      Serial.println(frontBreakPressureSecondByte);
-//      Serial.print("Fuel Pressure2: ");
-//      Serial.println(convert_kPa_psi(rawFuelPressure2));
-//      FPSecondByte=rawFuelPressure2;
+      packetData.frontBrakePressureSecondByte = convertHexToDec(in_buff);
     }
     if ((char)in_buff[0] == 'f') {
-//      Serial.println((char*)in_buff);
-      packetData.frontBreakPressureFirstByte = convertHexToDec(in_buff);
-//      Serial.println(frontBreakPressureFirstByte);
-//      Serial.print("Fuel Pressure1: ");
-//      Serial.println(convert_kPa_psi(rawFuelPressure1));
-//      FPFirstByte=rawFuelPressure1;
+      packetData.frontBrakePressureFirstByte = convertHexToDec(in_buff);
     }
-//    } else if ((char)in_buff[0] == 'f'){
-//      int rawFuelPressure1 = convertHexToDec(in_buff);
-//      Serial.print("Fuel Pressure1: ");
-//      Serial.println(convert_kPa_psi(rawFuelPressure1));
-//      FPFirstByte = rawFuelPressure1;
-//    }
     if ((char)in_buff[0] == 'r') {
-      packetData.rearBreakPressureFirstByte = convertHexToDec(in_buff);
+      packetData.rearBrakePressureFirstByte = convertHexToDec(in_buff);
     }
-
     if ((char)in_buff[0] == 'R') {
-      packetData.rearBreakPressureSecondByte = convertHexToDec(in_buff);
+      packetData.rearBrakePressureSecondByte = convertHexToDec(in_buff);
     }
 
-    if((packetData.frontBreakPressureFirstByte != 0) && (packetData.frontBreakPressureSecondByte != 0)){
-//      Serial.println(frontBreakPressureFirstByte);
-//      Serial.println(frontBreakPressureSecondByte);
-//      Serial.println(andrewsConversion(frontBreakPressureFirstByte, frontBreakPressureSecondByte));
-//      int andrewConv = andrewsConversion(frontBreakPressureFirstByte, frontBreakPressureSecondByte);
-//      Serial.println(convert_kPa_psi(andrewConv, 100);
-      Serial.print("Front Break Pressure: ");
-      Serial.println(convert_kPa_psi(andrewsConversion(packetData.frontBreakPressureFirstByte, packetData.frontBreakPressureSecondByte), 100));
-      packetData.frontBreakPressureFirstByte = 0;
-      packetData.frontBreakPressureSecondByte = 0;
+    if((packetData.throttlePositionFirstByte != 0) && (packetData.throttlePositionSecondByte != 0)){
+      Serial.print("Throttle Position: ");
+      Serial.println(convert_throttle(andrewsConversion(packetData.throttlePositionFirstByte, packetData.throttlePositionSecondByte), 100));
+      packetData.throttlePositionFirstByte = 0;
+      packetData.throttlePositionSecondByte = 0;
     }
-    if((packetData.rearBreakPressureFirstByte != 0) && (packetData.rearBreakPressureSecondByte != 0)){
-      Serial.print("Rear Break Pressure: ");
-      Serial.println(convert_kPa_psi(andrewsConversion(packetData.rearBreakPressureFirstByte, packetData.rearBreakPressureSecondByte), 100));
-      packetData.rearBreakPressureFirstByte = 0;
-      packetData.rearBreakPressureSecondByte = 0;
+
+    if((packetData.engineSpeedFirstByte != 0) && (packetData.engineSpeedSecondByte != 0)){
+      Serial.print("Engine Speed: ");
+      Serial.println(convert_Hz_rpm(andrewsConversion(packetData.engineSpeedFirstByte, packetData.engineSpeedSecondByte), 100));
+      packetData.engineSpeedFirstByte = 0;
+      packetData.engineSpeedSecondByte = 0;
     }
-    
 
+    if((packetData.fuelPressureFirstByte != 0) && (packetData.fuelPressureSecondByte != 0)){
+      Serial.print("Fuel Pressure: ");
+      Serial.println(convert_kPa_psi(andrewsConversion(packetData.fuelPressureFirstByte, packetData.fuelPressureSecondByte), 100));
+      packetData.fuelPressureFirstByte = 0;
+      packetData.fuelPressureSecondByte = 0;
+    }
 
-//    if((FPFirstByte != 0) && (FPSecondByte != 0)){
-//     
-//////      if (FPFirstByte > 0){
-////        Serial.print("Andrew's Fuel Pressure: ");
-////        Serial.println(((((FPFirstByte+16)/10) * 256) + ((FPSecondByte+16)/10))*0.0145);
-//
-//          Serial.print("Front Break Pressure");
-//          Serial.println(convert_kPa_psi(andrewsConversion(FPFirstByte, FPSecondByte),100));
-//
-////        Serial.print("JP's Fuel Pressure: ");
-////        Serial.println(convert_kPa_psi((((FPFirstByte+16)) * 256) + ((FPSecondByte+16)),100));
-////        Serial.println(convert_kPa_psi(andrewsConversion(FPFirstByte, FPSecondByte),100));
-////        Serial.print("First Byte: ");
-////        Serial.println(FPFirstByte);
-////        Serial.print("Second Byte: ");
-////        Serial.println(FPSecondByte);
-////        Serial.print("Break Pressure: (psi)");
-////
-////        Serial.print("First Byte:");
-////        Serial.println(FPFirstByte);
-////        Serial.print("Second Byte:");
-////        Serial.println(FPSecondByte);
-////        Serial.print("Just Andrews Conversion: ");
-////        Serial.println(andrewsConversion(-16, FPSecondByte));
-////        //Serial.print("Engine Speed:");
-////        //Serial.println(convert_Hz_rpm(andrewsConversion(FPFirstByte, FPSecondByte),100));
-//////         Serial.print("Coolant Temp (F):");
-////         Serial.println(convert_C_F(andrewsConversion(-16, FPSecondByte),10));
-//        //Serial.print("Raw Value from 
-//        //Serial.println(convert_kPa_psi(andrewsConversion(FPFirstByte, FPSecondByte),100));
-//        // Serial.print("Car Gear:");
-//        // Serial.println(convert_gear(andrewsConversion(-16, FPSecondByte)));
-////        Serial.print("Throttle Position:");
-////        Serial.println(convert_throttle(andrewsConversion(FPFirstByte, FPSecondByte),100));
-////        Serial.print("Exhaust Lambda:");
-////        Serial.println(convert_lambda(andrewsConversion(-16, FPSecondByte),1000));
-//        
-//
-//        
-//        
-////      
-//
-////      }
-////      else{
-////        Serial.print("Combined Fuel Pressure: ");
-////        Serial.println(convert_kPa_psi(FPSecondByte));
-////        Serial.print("Andrew's Number (2544): ");
-////        Serial.println((FPSecondByte)*0.0143);
-////        Serial.print("Andrew's Number (256): ");
-////        Serial.println((FPSecondByte)*0.0143);
-////      }
-//    }
-//      //Serial.println((char)in_buff[1]);
-//      //Serial.println((char)in_buff[2]);
-//      //Serial.println((char)in_buff[3]);
-////      Serial.println();
-////      convertHexToDec(in_buff);
-////      char A = (char)in_buff[1];
-////      char B = (char)in_buff[2];
-////      char C = (char)in_buff[3];
-////      int a = (A - '0');
-////      int b = (B - '0');
-////      int c = (C - '0');
-////      int d = (100*a + 10*b + c);
-//
-////      Serial.println(convertHexToDec(in_buff));
-////      int fuelPressureRaw = convertHexToDec(in_buff);
-//     
-////      Serial.println(convert_kPa_psi(fuelPressureRaw));
-//      //Serial.println(a);
-//      //Serial.println(b);
-//      //Serial.println(c);
-////      Serial.println(d);
-////      Serial.println();
-////      Serial.println(d/6.89475729)/100);
-////      Serial.println();
-//
-//     
-//
-//
-////    Serial.print((char)in_buff[1]);
-////    Serial.println("break");
-////    // (int)in_buff[1]*1000;
-////    Serial.print((int)in_buff[1]);
-////    Serial.println("break2");
-////    Serial.print((char)in_buff[3];
-//
-//    // say what you got:
-//    //Serial.print("I received: ");
-//    //Serial.println((char*)in_buff);
+    if((packetData.engineOilPressureFirstByte != 0) && (packetData.engineOilPressureSecondByte != 0)){
+      Serial.print("Engine Oil Pressure: ");
+      Serial.println(convert_kPa_psi(andrewsConversion(packetData.engineOilPressureFirstByte, packetData.engineOilPressureSecondByte), 100));
+      packetData.engineOilPressureFirstByte = 0;
+      packetData.engineOilPressureSecondByte = 0;
+    }
+
+    if(packetData.engineOilTemperatureByte != 0){
+      Serial.print("Engine Oil Temp: ");
+      Serial.println(convert_C_F(andrewsConversion(0, packetData.engineOilTemperatureByte), 100));
+      packetData.engineOilTemperatureByte = 0;
+    }
+
+    if(packetData.coolantTemperatureByte != 0){
+      Serial.print("Coolant Temp: ");
+      Serial.println(convert_C_F(andrewsConversion(0, packetData.coolantTemperatureByte), 100));
+      packetData.coolantTemperatureByte = 0;
+    }
+
+    if(packetData.gearByte != 0){
+      Serial.print("Gear: ");
+      Serial.println(convert_gear(andrewsConversion(0, packetData.gearByte)));
+      packetData.gearByte = 0;
+    }
+
+    if(packetData.exhaustLambdaByte != 0){
+      Serial.print("Exhaust Lambda: ");
+      Serial.println(convert_lambda(andrewsConversion(0, packetData.exhaustLambdaByte), 100));
+      packetData.exhaustLambdaByte = 0;
+    }
+
+    if((packetData.frontBrakePressureFirstByte != 0) && (packetData.frontBrakePressureSecondByte != 0)){
+      Serial.print("Front Brake Pressure: ");
+      Serial.println(convert_kPa_psi(andrewsConversion(packetData.frontBrakePressureFirstByte, packetData.frontBrakePressureSecondByte), 100));
+      packetData.frontBrakePressureFirstByte = 0;
+      packetData.frontBrakePressureSecondByte = 0;
+    }
+    if((packetData.rearBrakePressureFirstByte != 0) && (packetData.rearBrakePressureSecondByte != 0)){
+      Serial.print("Rear Brake Pressure: ");
+      Serial.println(convert_kPa_psi(andrewsConversion(packetData.rearBrakePressureFirstByte, packetData.rearBrakePressureSecondByte), 100));
+      packetData.rearBrakePressureFirstByte = 0;
+      packetData.rearBrakePressureSecondByte = 0;
+    }
     Serial.println();
   }
 }
