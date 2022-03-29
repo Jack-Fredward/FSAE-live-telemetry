68854495a83a8d30cac4356476d74b23a1ddbea0
Jack-Fredward <johnathan.prideaux@gmail.com>
2022-03-29 17:14:25 -0400

    working gui version 1

--- Python Software/live_telem_ui_main.py
+++ Python Software/live_telem_ui_main.py
@@ -1,3 +1,4 @@
+from posixpath import split
 import tkinter as tk
 from tkinter import *
 from tkinter import ttk
@@ -14,6 +15,8 @@ import numpy as np
 from random import seed
 from random import *
 import time
+import serial
+import datetime as dt
 
 
 """
@@ -41,45 +44,148 @@ LARGEFONT =("Verdana", 35)
 SMALLFONT =("calibre",10)
 # matplotlib graph styling
 # style.use("")
+ser = serial.Serial('COM3', 115200)
+ser.close()
+ser.open()
+
+xList = []
+
+throttle_position_data = []
+engine_speed_data = []
+fuel_pressure_data = []
+engine_oil_pressure_data = []
+engine_oil_temp_data = []
+coolant_temp_data = []
+gear_data = []
+exhaust_lambda_data = []
+front_brake_pressure_data = []
+rear_brake_pressure_data = []
+
 style.use("bmh")
 
 #format the graph with the given ylabel and ylimit axis
 def formatGraph(fig, yLabel, yLim):
 	fig.set_ylabel(yLabel,ha='right', rotation='horizontal')
-	fig.set_ylim(yLim)
+	# fig.set_ylim(yLim)
 
-#update the data to be displayed placeholder for now
-def updateData():
-	file = open("sampleData.txt", "r+")
-	x=len(file.readlines())+1
-	y = randint(0, 10)
-	file.write(str(x)+','+str(y)+'\n')
-	file.close()
-	return x
+# #update the data to be displayed placeholder for now
+# def updateData():
+# 	file = open("sampleData.txt", "r+")
+# 	x=len(file.readlines())+1
+# 	y = randint(0, 10)
+# 	file.write(str(x)+','+str(y)+'\n')
+# 	file.close()
+# 	return x
 
 #animate the graphs (make them live)
 #gets data and appends to display lists then plots and formats
-def animate(i, app):
-	xValue = updateData()
-	pullData = open("sampleData.txt","r").read()
-	dataList = pullData.split('\n')
-	xList = []
-	yList = []
-	for eachLine in dataList:
-		if len(eachLine)>1:
-			x, y = eachLine.split(',')
-			xList.append(int(x))
-			yList.append(int(y))
+def animate(i, xList, throttle_position_data,engine_speed_data,fuel_pressure_data,engine_oil_pressure_data,engine_oil_temp_data,coolant_temp_data,gear_data,exhaust_lambda_data,front_brake_pressure_data,rear_brake_pressure_data, app):
+	for i in range(10):
+		try:
+			raw_data = ser.readline().decode('utf-8')
+		except:
+			raw_data = ":0"
+		
+		try:
+			split_data = raw_data.split(':')
+			split_data[1].replace('\\','')
+			split_data[1].replace('r','')
+			split_data[1].replace('n','')
+		except:
+			print("cant split")
+		# print(split_data)
+		try:
+			data_value = float(split_data[1])
+		except:
+			data_value = 0
+
+		if split_data[0] == "Front Brake Pressure":
+			front_brake_pressure_data.append(data_value)
+		elif split_data[0] == "Rear Brake Pressure":
+			rear_brake_pressure_data.append(data_value)
+		elif split_data[0] == "Throttle Position":
+			throttle_position_data.append(data_value)
+		elif split_data[0] == "Engine Speed":
+			engine_speed_data.append(data_value)	
+		elif split_data[0] == "Fuel Pressure":
+			fuel_pressure_data.append(data_value)
+		elif split_data[0] == "Engine Oil Pressure":
+			engine_oil_pressure_data.append(data_value)
+		elif split_data[0] == "Engine Oil Temp":
+			engine_oil_temp_data.append(data_value)
+		elif split_data[0] == "Coolant Temp":
+			coolant_temp_data.append(data_value)
+		elif split_data[0] == "Exhaust Lambda":
+			exhaust_lambda_data.append(data_value)
+		elif split_data[0] == "Gear":
+			gear_data.append(data_value)
+
+
+
+	# front_brake_pressure = 0
+	# # print(split_data)
+	# if split_data[0] == "Front Brake Pressure":
+	# 	front_brake_pressure = split_data[1]
+	# 	print(split_data[1])
+	# 	try:
+	# 		front_brake_pressure = float(front_brake_pressure)
+	# 	except:
+	# 		print("error")
+	# 		front_brake_pressure=0
+
+	# 	yList[8].append(front_brake_pressure)
+	# 	yList = yList
+
+	# xValue = updateData()
+	# pullData = open("sampleData.txt","r").read()
+	# dataList = pullData.split('\n')
+	# xList = []
+	# yList = []
+	# for eachLine in dataList:
+		# if len(eachLine)>1:
+			# x, y = eachLine.split(',')
+	xList.append(dt.datetime.now().strftime('%H:%M:%S.%f'))
+	# yList.append(front_brake_pressure)
+
+	xList = xList[-20:]
+	# yList = yList[-20:]
+
+	throttle_position_data = throttle_position_data[-20:]
+	engine_speed_data = engine_speed_data[-20:]
+	fuel_pressure_data = fuel_pressure_data[-20:]
+	engine_oil_pressure_data = engine_oil_pressure_data[-20:]
+	engine_oil_temp_data = engine_oil_temp_data[-20:]
+	coolant_temp_data = coolant_temp_data[-20:]
+	gear_data = gear_data[-20:]
+	exhaust_lambda_data = exhaust_lambda_data[-20:]
+	front_brake_pressure_data = front_brake_pressure_data[-20:]
+	rear_brake_pressure_data = rear_brake_pressure_data[-20:]
+
+
+	data = [throttle_position_data, engine_speed_data, fuel_pressure_data, engine_oil_pressure_data, engine_oil_temp_data, coolant_temp_data, gear_data, exhaust_lambda_data, front_brake_pressure_data, rear_brake_pressure_data]
+	# for array in data:
+	# 	print(array)
+
+
 	i=0
-	colors = ['b', 'g', 'r', 'k', 'y','b', 'g', 'r', 'k',]
-	markers = ['o-','.-','o--','.--','s-','s:','*--','|-','_-']
+	colors = ['b', 'g', 'r', 'k', 'y','b', 'g', 'r', 'k','r']
+	markers = ['o-','.-','o--','.--','s-','s:','*--','|-','_-','.--']
+	# subplots = app.f.get_axes()
+
+
 	for subplot in app.f.get_axes():
 		
 		subplot.cla()
-		subplot.plot(xList[xValue-10:],yList[xValue-10:],colors[i]+markers[i])
+		try:
+			subplot.plot(xList,data[i],colors[i]+markers[i])
+		except:
+			print("cant plot")
 		# subplot.plot(xList,yList,colors[i]+markers[i])
 		i=i+1
 	
+
+	
+
 	#if i put all the titles in a list then i can just use the for loop above to do all the formatting
 	#format Graphs
 	formatGraph(app.ax1, "Throttle Position", (-1,11))
@@ -90,7 +196,8 @@ def animate(i, app):
 	formatGraph(app.ax6, "Coolant Temp", (0,5))
 	formatGraph(app.ax7, "Gear", (0,10))
 	formatGraph(app.ax8, "Exhaust Lambda", (5,10))
-	formatGraph(app.ax9, "Break Pressure", (0,10))
+	formatGraph(app.ax9, "Front Brake Pressure", (0,10))
+	formatGraph(app.ax10, "Rear Brake Pressure", (1,1))
 	# formatGraph(app.ax1, "Ax1's \ngraph \ny label", (-1,11))
 	# formatGraph(app.ax2, "Ax2's \ngraph \ny label", (0,10))
 	# formatGraph(app.ax3, "Ax3's \ngraph \ny label", (0,10))
@@ -100,11 +207,12 @@ def animate(i, app):
 	# formatGraph(app.ax7, "Ax7's \ngraph \ny label", (0,10))
 	# formatGraph(app.ax8, "Ax8's \ngraph \ny label", (5,10))
 	# formatGraph(app.ax9, "Ax9's \ngraph \ny label", (0,10))
-	app.ax9.set_xlabel("Time(s)")
+	app.ax10.set_xlabel("Time(s)")
 	# app.ax9.set_xlabel("Data Point")
 	# app.ax9.set_xticks(rotation=45)
-	plt.setp(app.ax9.get_xticklabels(), rotation=45, ha="right",
+	plt.setp(app.ax10.get_xticklabels(), rotation=45, ha="right",
          rotation_mode="anchor")
+
 
 
 class LiveTelemUI(tk.Tk):
@@ -116,7 +224,7 @@ class LiveTelemUI(tk.Tk):
 
 		#figure
 		self.f = Figure()
-		self.f, ((self.ax1, self.ax2, self.ax3, self.ax4,self.ax5, self.ax6, self.ax7, self.ax8, self.ax9)) = plt.subplots(9,1, sharex=True)
+		self.f, ((self.ax1, self.ax2, self.ax3, self.ax4,self.ax5, self.ax6, self.ax7, self.ax8, self.ax9, self.ax10)) = plt.subplots(10,1, sharex=True)
 
 		# creating a container 
 		container = tk.Frame(self) 
@@ -183,8 +291,9 @@ def main():
 	app.geometry("1280x760")
 	#start in fullscreen windowed
 	# app.state('zoomed')
-	ani = animation.FuncAnimation(app.f, animate,fargs = (app,),interval=500)
+	ani = animation.FuncAnimation(app.f, animate,fargs = (xList, throttle_position_data,engine_speed_data,fuel_pressure_data,engine_oil_pressure_data,engine_oil_temp_data,coolant_temp_data,gear_data,exhaust_lambda_data,front_brake_pressure_data,rear_brake_pressure_data, app,),interval=500)
 	app.mainloop()
 	
 
 main()
+
